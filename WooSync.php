232f0fbfff406cf0ea3e33f81859bc515b06155d
tableros <40235617+yevea@users.noreply.github.com>
2026-01-18 16:15:26 +0100

    Create WooSync plugin class with init and update methods

---
+++ WooSync.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace FacturaScripts\Plugins\WooSync;
+
+use FacturaScripts\Core\Base\InitClass;
+
+class WooSync extends InitClass
+{
+    public function init() {}
+    public function update() {}
+}
